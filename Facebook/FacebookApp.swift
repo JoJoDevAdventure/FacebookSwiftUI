6010229260174b5c252c99f7e7507c1e3beabc69
Youssef <youssef.b.air2@gmail.com>
2021-12-24 19:27:54 +0100

    Initial Commit

---
+++ Facebook/FacebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookApp.swift
+//  Facebook
+//
+//  Created by Youssef Bhl on 24/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct FacebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
